3f5d6c6540ff2ee4aeebcf2a24aa0e5c4ea71ed7
Vivek <vivekkv178@gmail.com>
2024-05-10 07:45:36 +0800

    Bug Test 3

---
+++ stories/Blank.stories.tsx
@@ -0,0 +1,10 @@
+import { Meta } from "@storybook/react";
+
+export default {
+  title: "HiddenStories",
+  parameters: {
+    hidden: true,
+  },
+} as Meta;
+
+export const BlankStory = () => null;
